2b5a3c6c5a91e6343103b215f53df86bd7153c02
student <student@info-24.cshl.edu>
2022-10-11 16:38:56 -0400

    get fancy with TF script

--- python2_problemset/value_TF.py
+++ python2_problemset/value_TF.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
-a = 9
-if a == True:
-	print(a, "is True")
+import sys
+
+a = sys.argv[1]
+
+try:
+	if type(int(a)) == int:
+		b = int(a)
+	else:
+		b = a
+except:
+	try:
+		if type(str(a)) == str:
+			b = str(a)
+		else:
+			b = a
+	except:
+		try:
+			if type(bool(a)) == bool:
+				b = bool(a)
+			else:
+				b = a
+		except:
+			b = a
+
+if bool(b) == True:
+	print(b, "is True")
 else:
-	print(a, "is not True")
+	print(b, "is not True")
