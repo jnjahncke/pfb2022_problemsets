e015c919b942e5ec96a2dd24d4e95c74cef9c8ff
student <student@info-24.cshl.edu>
2022-10-11 15:18:35 -0400

    ifelse practice

---
+++ python2_problemset/pos_neg_50.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+a = 21
+message = ""
+if a > 0:
+	message = "positive"
+	print(message)
+elif a < 0:
+	message = "negative"
+	print(message)
+else:
+	message = "zero"
+	print(message)
+
