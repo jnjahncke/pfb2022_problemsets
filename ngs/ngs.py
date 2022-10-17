2fb0e5fffd5a1db7bbd883d54b7996dcaedde3d3
student <student@info-24.cshl.edu>
2022-10-16 21:52:40 -0400

    fastq qc in progress

--- ngs/ngs.py
+++ ngs/ngs.py
@@ -2,6 +2,7 @@
 
 from Bio import SeqIO
 import re
+import sys
 
 genome_file = "Ecoli.fasta"
 annotation_file = "Ecoli.gff3"
@@ -42,3 +43,37 @@ print(f"The phred characters used are: {qual_raw_set}")
 print(f"The converted numbers are: {qual_to_ASCII}")
 # The minimum ASCII should indicate the offset
 print(f"Because the minimum offset is {min(qual_to_ASCII)}, the offset can be assumed to be +33")
+
+
+# Trim poor-quality bases from the ends of the FASTQ sequences and output new FASTQ
+file_in = sys.argv[1]
+threshold = int(sys.argv[2])
+
+with open(file_in,"r") as reads, open(file_in[:-6]+"_qc.fastq","w") as fastq_out:
+	linenum = 0
+	for line in reads:
+		linenum += 1
+		start_1 = ""
+		read_2 = ""
+		plus_3 = ""
+		qual_4 = ""
+		qual_trans = ""
+		qual_list = []
+		line = line.rstrip()
+		if line[0] == "@":
+			start_1 = line
+		if linenum % 2 == 0 and linenum % 4 != 0:
+			read_2 = line
+		if line[0] == "+":
+			plus_3 = line
+		else:
+			qual_4 = line
+			qual_4_temp = list(line)
+			for char in qual_4_temp:
+				if ord(char)-33 > threshold:
+					qual_list.append(ord(char))
+			qual_len = len(qual_list)*(-1)
+		fastq_out.write(f'''{start_1}
+{read_2[:qual_len]}
+{plus_3}
+{qual_4[:qual_len]}''')
