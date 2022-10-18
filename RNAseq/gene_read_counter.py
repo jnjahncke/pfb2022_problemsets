5fe45288b369d77827685c0d2218e00d177fdc49
student <student@info-24.cshl.edu>
2022-10-18 15:38:27 -0400

    fix gene read counter

--- RNAseq/gene_read_counter.py
+++ RNAseq/gene_read_counter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-import re
 
 ## Read bowtie2.sam and generate a table containing the number of reads mapped to each gene
 
@@ -12,15 +11,19 @@ sam_file = sys.argv[1]
 #	Get gene name
 #	Count number of reads for that gene
 gene_counts = {}
+reads = []
 with open(sam_file, "r") as mapped:
 	for line in mapped:
 		line = line.rstrip()
-		for found in re.finditer(r"\s([!-~]+?)\^", line):
-			gene_name = found.group(1)
-		if gene_name not in gene_counts:
-			gene_counts[gene_name] = 1
-		else:
-			gene_counts[gene_name] += 1
-
+		gene_name = line.split("\t")[2]
+		gene_name = gene_name.split("^")[0]
+		readID = line.split("\t")[0]
+		if readID not in reads: # Only count each read once per gene
+			reads.append(readID)
+			if gene_name not in gene_counts:
+				gene_counts[gene_name] = 1
+			else:
+				gene_counts[gene_name] += 1
+			
 for k,v in gene_counts.items():
-	print(f'{k:<8} {v:>6}')
+	print(f'{k:<11} {v:>6}')
